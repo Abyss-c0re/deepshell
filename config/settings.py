2802d2232260125266ca6467d6b89ffe42b994b6
catoni0 <mr.vladimir1337@gmail.com>
2025-04-05 22:45:20 +0300

    Switched helper model and increased limits for files and shell output

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ DEFAULT_MODEL = "phi4:14b"
 CODE_MODEL = "deepseek-coder-v2:16b"
 SHELL_MODEL = "granite3.2:8b"
 SYSTEM_MODEL = "granite3.2:8b"
-HELPER_MODEL = "granite3.2:2b"
+HELPER_MODEL = "deepseek-r1:1.5b"
 VISION_MODEL = "minicpm-v:8b"
 EMBEDDING_MODEL = "nomic-embed-text:latest"
 
@@ -34,8 +34,8 @@ MODE_CONFIGS = {
 #Logging
 LOG = True
 LOG_LEVEL = "info" #Possible values: debug, info, warning, error, critical
-LOG_TO_FILE = True
-LOG_TO_UI = False
+LOG_TO_FILE = False
+LOG_TO_UI = True
 
 #Rendering
 RENDER_DELAY = 0.0069 # Delay between rendering lines
@@ -52,7 +52,7 @@ SLICE_SIZE = 4 # Last N messages to analyze for off-topic
 SHELL_TYPE = "/bin/bash"
 MONITOR_INTERVAL = 60 # Timeout until when user will be prompted to abort command execution 
 FINALIZE_OUTPUT = True # Output post-processing such as trimming
-MAX_OUTPUT_LINES = 1000
+MAX_OUTPUT_LINES = 30000
 
 #FileProcessing Config 
 PROCESS_IMAGES = False # Turn this on if you want to get a description of the images
@@ -60,8 +60,7 @@ IMG_INPUT_RES = (512, 512)
 
 IGNORE_DOT_FILES = False
 MAX_FILE_SIZE = 6 * 1024 * 1024 #6MB
-MAX_LINES = 1000
-CHUNK_SIZE = 4000
+MAX_LINES = 30000
 
 # General text formats
 TEXT_FORMATS = [
